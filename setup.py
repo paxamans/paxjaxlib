8db331ce77bd88c0cdfd77f81f3f4ea83b9af71e
paxamans <paxamans@gmail.com>
2025-12-14 23:37:02 +0100

    version correction

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="paxjaxlib",
-    version="0.2.0",
+    version="0.1.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=["jax", "jaxlib", "numpy"],
